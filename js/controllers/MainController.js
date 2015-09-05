cc758d8993061c87e9fae8de5bc85920263edd5c
Anni Vaananen <anni.vaananen@gmail.com>
2015-09-05 12:53:33 +0100

    something tharr

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,5 +1,5 @@
 app.controller('MainController', ['$scope', function($scope){
-  var headers = ["Web Developer","Bachelor's degree in Marketing & PR" , "Background from Accounting & Bokkeeping",  "Vast experience from event technology", "Sailing, Cycling & Bouldering <3"];
+  var headers = ["Web Developer","Bachelor's degree in Marketing & PR", "Background from Accounting & Bokkeeping",  "Experience from event technology", "Sailing, Cycling & Bouldering <3"];
   var show = 0;
   $scope.title = headers[show];
 
